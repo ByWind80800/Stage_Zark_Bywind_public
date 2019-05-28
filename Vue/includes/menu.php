864f1bc0a8b6bf441dad68382bc5e6b6e0655bf9
ByWind80800 <labouro.Loic@hotmail.fr>
2019-05-28 16:40:09 +0200

    update menu

--- Vue/includes/menu.php
+++ Vue/includes/menu.php
@@ -1,14 +1,14 @@
 <section>
 	<!-- Barre de navigation -->
 		<div>
-			<ul class="nav navbar-secondary bg-secondary">
+			<ul class="barrenav">
 		      	<li><a class="lien" href="index.php?page=Controler&param=Accueil">Accueil</a></li>
 				<li><a class="lien" href="#">Actualité</a></li>
 				<li><a class="lien" href="#">Commune</a></li>
 				<li><a class="lien" href="#">Services</a></li>
 				<li><a class="lien" href="#">Associations</a></li>
 				<li><a class="lien" href="index.php?page=Controler&param=Connexion">Connexion</a></li>
-				<li><img src="Vue/img/logo.png" class="justify-content-end"></li>
+				<img src="Vue/img/logo.png" align="right" class="logo">
 		    </ul>
 		    <br/>
 		</div>
