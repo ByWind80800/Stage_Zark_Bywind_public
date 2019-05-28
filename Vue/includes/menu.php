48825efde0b593590f05626077d7a5e6ea3558ab
Zark43 <kevin.pascal25@gmail.com>
2019-05-28 16:14:22 +0200

    Changement menu

--- Vue/includes/menu.php
+++ Vue/includes/menu.php
@@ -1,13 +1,14 @@
 <section>
 	<!-- Barre de navigation -->
 		<div>
-			<ul class="nav navbar-dark bg-dark">
-		      	<li><a class="lien" href="index.php?page=Controler&param=Accueil"><img src="Vue/img/logo.png" class="imglogo"></a></li>
+			<ul class="nav navbar-secondary bg-secondary">
+		      	<li><a class="lien" href="index.php?page=Controler&param=Accueil">Accueil</a></li>
 				<li><a class="lien" href="#">Actualité</a></li>
 				<li><a class="lien" href="#">Commune</a></li>
 				<li><a class="lien" href="#">Services</a></li>
 				<li><a class="lien" href="#">Associations</a></li>
 				<li><a class="lien" href="index.php?page=Controler&param=Connexion">Connexion</a></li>
+				<li><img src="Vue/img/logo.png" class="justify-content-end"></li>
 		    </ul>
 		    <br/>
 		</div>
