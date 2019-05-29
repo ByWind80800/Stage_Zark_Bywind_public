6c4367539a845680344f3171be82b9979e8f9fb0
Zark43 <kevin.pascal25@gmail.com>
2019-05-29 09:45:47 +0200

    Changement

--- Vue/includes/menu.php
+++ Vue/includes/menu.php
@@ -3,11 +3,54 @@
 		<div>
 			<ul class="barrenav">
 		      	<li><a class="lien" href="index.php?page=Controler&param=Accueil">Accueil</a></li>
-				<li><a class="lien" href="#">Actualité</a></li>
-				<li><a class="lien" href="#">Commune</a></li>
-				<li><a class="lien" href="#">Services</a></li>
-				<li><a class="lien" href="#">Associations</a></li>
-				<li><a class="lien" href="index.php?page=Controler&param=Connexion">Connexion</a></li>
+
+				<li class="nav-item dropdown">
+				   	<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Actualités</a>
+				    <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">Nouvelles</a>
+				    	<a class="dropdown-item" href="#">Agenda</a>
+				    	<a class="dropdown-item" href="#">Rétrophotos</a>
+				    </div>
+				</li>
+
+				<li class="nav-item dropdown">
+				   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Commune</a>
+				   <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">Mairie</a>
+				    	<a class="dropdown-item" href="#">Conseil Municipal</a>
+				    	<a class="dropdown-item" href="#">Équipement Municipaux</a>
+				    </div>
+				</li>
+
+				<li class="nav-item dropdown">
+				   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+				   <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">Espace Numérique</a>
+				    	<a class="dropdown-item" href="#">Ramassage des Ordures Ménagères</a>
+				    	<a class="dropdown-item" href="#">Eau Potable</a>
+				    	<a class="dropdown-item" href="#">Salle Communale</a>
+				    	<a class="dropdown-item" href="#">Entreprises et Sociétés</a>
+				    </div>
+				</li>
+
+				<li class="nav-item dropdown">
+				   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Association</a>
+				   <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">Comité des Fêtes</a>
+				    	<a class="dropdown-item" href="#">Association de la Chasse</a>
+				    	<a class="dropdown-item" href="#">Association de la Pétanque de la Tour</a>
+				    </div>
+				</li>
+
+				<li class="nav-item dropdown">
+				   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Découverte</a>
+				   <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">Histoire</a>
+				    	<a class="dropdown-item" href="#">Patrimoine</a>
+				    	<a class="dropdown-item" href="#">Parcours de Freycenet</a>
+				    </div>
+				</li>
+
 				<img src="Vue/img/logo.png" align="right" class="logo">
 		    </ul>
 		    <br/>
