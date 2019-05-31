aefa6487a22ae1e115b315031b9c072092709175
Zark43 <kevin.pascal25@gmail.com>
2019-05-31 08:34:22 +0200

    Update Controler.php

--- Controler/Controler.php
+++ Controler/Controler.php
@@ -12,98 +12,116 @@ if(isset($_REQUEST['param']))
         ////////////////////////////////////////////////////////////////////////////////////// Action renvoie des pages
 		case 'Accueil' : 
 		{
-			include (dirname(__FILE__).'/../Vue/includes/accueil.php');
+			include (dirname(__FILE__).'/../Vue/includes/Accueil.php');
 			break;
 		}
 
 		case 'Connexion':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/connexion.php');
+			break;
 		}
 
 		case 'nouvelles':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Actualités/nouvelles.php');
+			break;
 		}
 
 		case 'nouvelles':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Actualités/agenda.php');
+			break;
 		}
 
 		case 'nouvelles':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Actualités/retrophotos.php');
+			break;
 		}
 
 		case 'Mairie':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Commune/Mairie.php');
+			break;
 		}
 
 		case 'ConseilMunicipal':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Commune/ConseilMunicipal.php');
+			break;
 		}
 
 		case 'travaux':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Commune/travaux.php');
+			break;
 		}
 
 		case 'EspaceNum':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Services/EspaceNum.php');
+			break;
 		}
 
 		case 'RamassOm':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Services/RamassOm.php');
+			break;
 		}
 
 		case 'Eau':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Services/Eau.php');
+			break;
 		}
 
 		case 'SalleCommunale':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Services/SalleCommunale.php');
+			break;
 		}
 
 		case 'Societe':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Services/Societe.php');
+			break;
 		}
 
 		case 'comiteFete':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Association/comiteFete.php');
+			break;
 		}
 
 		case 'Chasse':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Association/Chasse.php');
+			break;
 		}
 
 		case 'Petanque':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Association/Petanque.php');
+			break;
 		}
 
 		case 'histoire':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Decouverte/histoire.php');
+			break;
 		}
 
 		case 'patrimoine':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Decouverte/patrimoine.php');
+			break;
 		}
 
 		case 'Parcours':
 		{
 			include(dirname(__FILE__).'/../Vue/includes/Decouverte/Parcours.php');
+			break;
 		}
 
 
