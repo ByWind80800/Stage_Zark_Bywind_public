93a03afd614103c3e83516ec66d603efda197bbd
Zark43 <kevin.pascal25@gmail.com>
2019-05-31 10:58:14 +0200

    Update Mairie.php

--- Vue/includes/Commune/Mairie.php
+++ Vue/includes/Commune/Mairie.php
@@ -1,14 +1,21 @@
 <div class="container">
-			<div align="center">
-				<div class="row">
-					<div class="col-md-6">
-						<img src="">
-					</div>
-					<div class="col-md-6">
-						<p>Horaires d'ouverture</p>
-						<p>Le lundi de 10h00 à 12h00 et de 14h00 à 16h00 <br/>
-						Le jeudi de 10h00 à 12h00 et de 14h00 à 16h00</p>
-					</div>
-				</div>
+	<div align="center">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="">
+			</div>
+			<div class="col-md-6">
+				<table border="2">
+					<tr><td><font face="papyrus"><b>Lundi</b></font></td><td><pre><font face="papyrus"><b>10h-12h		14h-16h</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Mardi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Mercredi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Jeudi</b></font></td><td><pre><font face="papyrus"><b>10h-12h		14h-16h</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Vendredi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Samedi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+					<tr><td><font face="papyrus"><b>Dimanche</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+				</table>
+				<br />
 			</div>
 		</div>
+	</div>
+</div>
