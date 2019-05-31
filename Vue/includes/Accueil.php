0fd8d8508602ec408776a167331b59724da0dffb
ByWind80800 <labouro.Loic@hotmail.fr>
2019-05-31 08:55:40 +0200

    Finition menu et css menu

--- Vue/includes/Accueil.php
+++ Vue/includes/Accueil.php
@@ -1,23 +1,5 @@
 <section>
 	<!-- Développement 1 -->
 	<h2 class="soustitre">Bienvenue sur le site de Freycenet la tour</h2>
-	
 
-
-
-
-		<!-- <div class="soustitre">
-			<h2>Présentation de la ville</h2>
-		</div>
-		<div class="carrousel">
-			<h2>Carrousel / galerie photo</h2>
-			<div class="fleche">
-				Flèche directionnelle pour voir les photos
-			</div>
-		</div>
-		<p></p>
-		<div class="soustitre">
-			<h2>Histoire de la ville</h2>
-		</div>
-		<p></p> -->
 </section>
