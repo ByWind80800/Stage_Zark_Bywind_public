b8f8f5793bdffdd3ed6b3a02c9f50e7c1ae789a1
ByWind80800 <labouro.Loic@hotmail.fr>
2019-05-31 11:17:28 +0200

    Ajout Tableau
    Dans la page Mairie

--- Vue/includes/Commune/Mairie.php
+++ Vue/includes/Commune/Mairie.php
@@ -6,13 +6,58 @@
 			</div>
 			<div class="col-md-6">
 				<table border="2">
-					<tr><td><font face="papyrus"><b>Lundi</b></font></td><td><pre><font face="papyrus"><b>10h-12h		14h-16h</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Mardi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Mercredi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Jeudi</b></font></td><td><pre><font face="papyrus"><b>10h-12h		14h-16h</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Vendredi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Samedi</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
-					<tr><td><font face="papyrus"><b>Dimanche</b></font></td><td><pre><font face="papyrus"><b>		Fermé	</b></font></pre></td></tr>
+					<tr>
+						<th colspan="1">Jours</th>
+						<th colspan="3">Horaires</th>
+					</tr>
+					<tr>
+						<td><font face="papyrus"><b>Lundi</b></font></td>
+						<td><font face="papyrus"><b>10h-12h</b></font></td>
+						<td><font face="papyrus"><b></b></font></td>
+						<td><font face="papyrus"><b>14h-16h</b></font></td>	
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Mardi</b></font></td>
+						<td></td>
+						<td><font face="papyrus"><b>Fermé</b></font></td>
+						<td></td>
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Mercredi</b></font></td>
+						<td></td>
+						<td><font face="papyrus"><b>Fermé</b></font></td>
+						<td></td>
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Jeudi</b></font></td>
+						<td><font face="papyrus"><b>10h-12h</b></font></td>
+						<td><font face="papyrus"><b></b></font></td>
+						<td><font face="papyrus"><b>14h-16h</b></font></td>				
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Vendredi</b></font></td>
+						<td></td>
+						<td><font face="papyrus"><b>Fermé</b></font></td>
+						<td></td>
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Samedi</b></font></td>
+						<td></td>
+						<td><font face="papyrus"><b>Fermé</b></font></td>
+						<td></td>
+					</tr>
+
+					<tr>
+						<td><font face="papyrus"><b>Dimanche</b></font></td>
+						<td></td>
+						<td><font face="papyrus"><b>Fermé</b></font></td>
+						<td></td>
+					</tr>
 				</table>
 				<br />
 			</div>
