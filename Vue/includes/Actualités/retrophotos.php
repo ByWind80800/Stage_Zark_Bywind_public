1d76e39dcf818fee59cafc83e0edb4c2f244c796
Zark43 <kevin.pascal25@gmail.com>
2019-06-03 15:48:53 +0200

    Changement

--- Vue/includes/Actualités/retrophotos.php
+++ Vue/includes/Actualités/retrophotos.php
@@ -1,19 +1,74 @@
 <section>
-	<h2>Photos Éthan des Barthes</h2>
+	<h2>Photos étang des Barthes</h2>
 	<div class="container">
 		<div align="center">
 			<div class="row">
 				<div class="col-md-3">
-					<img src="Vue/img/f0277034.jpg" width="100%" height="100%">
+					<img src="Vue/img/photos_paysage/les_Barthes/f0277034.jpg" width="100%" height="100%">
 				</div>
 				<div class="col-md-3">
-					<img src="Vue/img/f0357034.jpg" width="100%" height="100%">
+					<img src="Vue/img/photos_paysage/les_Barthes/f0357034.jpg" width="100%" height="100%">
 				</div>
 				<div class="col-md-3">
-					<img src="Vue/img/f0372202.jpg" width="100%" height="100%">
+					<img src="Vue/img/photos_paysage/les_Barthes/f0372202.jpg" width="100%" height="100%">
 				</div>
 				<div class="col-md-3">
-					<img src="Vue/img/f0578154.jpg" width="100%" height="100%">
+					<img src="Vue/img/photos_paysage/les_Barthes/f0578154.jpg" width="100%" height="100%">
+				</div>
+			</div>
+		</div>
+	</div>
+	<h2>Les sucs autour de Freycenet la Tour</h2>
+	<div class="container">
+		<div align="center">
+			<div class="row">
+				<div class="col-md-4">
+					<img src="Vue/img/photos_paysage/les_sucs_autour_de_freycenet_la_tour/f0538666.jpg" width="100%" height="100%">
+				</div>
+				<div class="col-md-4">
+					<img src="Vue/img/photos_paysage/les_sucs_autour_de_freycenet_la_tour/f0542250.jpg" width="100%" height="100%">
+				</div>
+				<div class="col-md-4">
+					<img src="Vue/img/photos_paysage/les_sucs_autour_de_freycenet_la_tour/f1453482.jpg" width="100%" height="100%">
+				</div>
+			</div>
+		</div>
+	</div>
+	<h2>Photos paysage Freycenet la Tour</h2>
+	<div class="container">
+		<div align="center">
+			<div class="row">
+				<div class="col-md-3">
+					<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f0436074.jpg" width="100%" height="75%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f0437290.jpg" width="100%" height="75%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f1416234.jpg" width="100%" height="75%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f1391914.jpg" width="100%" height="75%">
+				</div>
+			</div>
+		</div>
+	</div>
+	<h2>L'Église de Freycenet la Tour</h2>
+	<div class="container">
+		<div align="center">
+			<div class="row">
+				<div class="col-md-12">
+					<img src="Vue/img/photos_paysage/église de la mairie.jpg" width="100%" height="100%">
+				</div>
+			</div>
+		</div>
+	</div>
+	<p></p>
+	<div class="container">
+		<div align="center">
+			<div class="row">
+				<div class="col-md-12">
+					<img src="Vue/img/photos_paysage/Eglise.jpg" width="100%" height="100%">
 				</div>
 			</div>
 		</div>
