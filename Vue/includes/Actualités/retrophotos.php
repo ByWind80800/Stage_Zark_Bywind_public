9421e4f87e5b1c3874a7e5170b38b7f4973b7cf4
Zark43 <kevin.pascal25@gmail.com>
2019-06-03 14:32:07 +0200

    Changement

--- Vue/includes/Actualités/retrophotos.php
+++ Vue/includes/Actualités/retrophotos.php
@@ -4,7 +4,16 @@
 		<div align="center">
 			<div class="row">
 				<div class="col-md-3">
-					
+					<img src="Vue/img/f0277034.jpg" width="100%" height="100%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/f0357034.jpg" width="100%" height="100%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/f0372202.jpg" width="100%" height="100%">
+				</div>
+				<div class="col-md-3">
+					<img src="Vue/img/f0578154.jpg" width="100%" height="100%">
 				</div>
 			</div>
 		</div>
