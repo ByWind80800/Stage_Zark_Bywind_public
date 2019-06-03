8066aa069cf89661180696cfd0876a0e44cbfcc5
ByWind80800 <labouro.Loic@hotmail.fr>
2019-06-03 09:29:44 +0200

    Ajout ressource pack et modif MCD

---
+++ Vue/includes/Services/Societe.php
@@ -0,0 +1,9 @@
+<section>
+	<table border="2" class="tableau">
+		<tr>
+			<th>Nom Entreprise</th>
+			<th>Nom du gérant</th>
+			<th>Corps de métier de l'entreprise</th>
+		</tr>		
+	</table>
+</section>
