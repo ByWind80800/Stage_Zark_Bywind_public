130a7ba9f07be6b47b5a1ce57b9805db3248035f
DJprojet <48954245+DJprojet@users.noreply.github.com>
2019-07-30 18:54:14 +0200

    modif accueil

--- Vue/includes/Accueil.php
+++ Vue/includes/Accueil.php
@@ -1,28 +1,8 @@
 <section>
 	<h2 class="soustitre">Bienvenue sur le site de Freycenet la tour</h2>
-
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner">
-    	<div class="carousel-item active">
-      		<img class="d-block w-100" src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f1416234.jpg" alt="First slide">
-    	</div>
-    	<div class="carousel-item">
-      		<img class="d-block w-100" src="..." alt="Second slide">
-    	</div>
-    	<div class="carousel-item">
-      		<img class="d-block w-100" src="..." alt="Third slide">
-    	</div>
-  	</div>
-  	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    	<span class="sr-only">Previous</span>
-  	</a>
-  	<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
-    	<span class="sr-only">Next</span>
-  	</a>
-</div>
-
+	<div align="center">
+		<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f1416234.jpg" width="1280" height="720" alt="First slide">
+	</div>
 	<div class="container">
 		<div align="center">
 			<h4 class="soustitre2">Données sur Freycenet la tour</h4>
