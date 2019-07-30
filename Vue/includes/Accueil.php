10b9f4fc40c97b0bc65e1d575195935ee4e62ac5
DJprojet <48954245+DJprojet@users.noreply.github.com>
2019-07-30 22:55:33 +0200

    Update Accueil.php

--- Vue/includes/Accueil.php
+++ Vue/includes/Accueil.php
@@ -1,9 +1,12 @@
 <section>
 	<h2 class="soustitre">Bienvenue sur le site de Freycenet la tour</h2>
 <<<<<<< HEAD
+<<<<<<< HEAD
 	<div  class="imgAccueil" align="center">
 		<img src="Vue/img/photos_paysage/photos_paysage_freycenet_la_tour/f1416234.jpg" width="95%" height="95%">
 	</div>
+=======
+>>>>>>> parent of 130a7ba... modif accueil
 =======
 >>>>>>> parent of 130a7ba... modif accueil
 
